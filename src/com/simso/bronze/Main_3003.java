333a15a0b882250de32cacd1d303b521f00def1f
dnflsclsrn1 <wnsgur765z@gmail.com>
2021-10-27 09:19:49 +0900

    feat: bronze 문제추가

---
+++ src/com/simso/bronze/Main_3003.java
@@ -0,0 +1,32 @@
+package com.simso.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main_3003 {
+
+    public static void main(String arg[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        int chess[] = new int[]{1, 1, 2, 2, 2, 8};
+
+
+        for (int i = 0; i < 6; i++) {
+
+            int piece = Integer.parseInt(st.nextToken());
+
+
+            System.out.print(chess[i] - piece + " ");
+        }
+
+    }
+
+
+}
---
+++ src/com/simso/bronze/Main_5337.java
@@ -0,0 +1,12 @@
+package com.simso.bronze;
+
+public class Main_5337 {
+
+    public static void main(String arg[]) {
+
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+
+    }
+}
