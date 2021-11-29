baa34ae54383fe10a5dece91dd2b090d692a599b
dnflsclsrn1 <wnsgur765z@naver.com>
2021-11-29 17:32:50 +0900

    feat bronze sol 2 add

---
+++ src/com/simso/bronze/bromze4/Main_10179.java
@@ -0,0 +1,29 @@
+package com.simso.bronze.bromze4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main_10179 {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        double[] array = new double[T];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Double.parseDouble(br.readLine()) * 0.8;
+
+        }
+
+        for (double n: array) {
+            System.out.println("$"+ String.format("%.2f", n));
+
+        }
+        br.close();
+    }
+}
+
+
--- src/com/simso/bronze/bromze4/Main_11948.java
+++ src/com/simso/bronze/bromze4/Main_11948.java
@@ -1,29 +1,19 @@
 package com.simso.bronze.bromze4;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
-import java.util.StringTokenizer;
-
 
 public class Main_11948 {
 
     public static void main(String[] args) throws Exception {
 
-        Scanner scan = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        int a = scan.nextInt();
-        int res = (a / 10) + (a % 10);
+        String r = sc.next();
 
-        if (a / 10 > 10) {
-            res = (a / 100) + (a % 100);
-        }
-        if (a == 1010) {
-            res = 20;
-        }
+        int y = Character.getNumericValue(r.charAt(0));
+        int x = Character.getNumericValue(r.charAt(1));
 
-        System.out.println(res);
-
+        System.out.println(x+y);
     }
 
 
